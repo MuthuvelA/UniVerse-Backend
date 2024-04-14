8763df9fc53d9bfcc6b3f4824519a30f95ba1434
MuthuvelA <muthuveldiary@gmail.com>
2024-04-14 15:03:12 +0530

    basic architecture setup done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
-const userRouter = require('./router/userRouter');
+const userRouter = require('./router/userrouter');
 const app = express();
 
 app.use(cors());
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const app = require("./app");
 const port = process.env.PORT || 3002;
 
-app.get('/',(req,res)=>{
-     res.send("sdfa");
-});
-
 app.listen(port,()=>{
     console.log(`Server Listening on Port http://localhost:${port}`);
 });
